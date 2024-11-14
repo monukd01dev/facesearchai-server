21f8d83041c670a57ea064e61c87838d2360ed7b
monukdjavadev <monukd01dev@gmail.com>
2024-11-14 21:03:47 +0530

    vercel

--- config.js
+++ config.js
@@ -1,11 +1,18 @@
 const { Pool } = require("pg");
 
+// const pool = new Pool({
+// 	user: "postgres",
+// 	host: "localhost",
+// 	database: "facesearchdb",
+// 	password: "KDisArtist",
+// 	port: 5432,
+// });
+
 const pool = new Pool({
-	user: "postgres",
-	host: "localhost",
-	database: "facesearchdb",
-	password: "KDisArtist",
-	port: 5432,
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 });
 
 module.exports = pool;
